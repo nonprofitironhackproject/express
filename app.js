2c82ef888804c4b57cc2ca17ddebe6fd5ae07835
Ryan Martin <rmart106@fiu.edu>
2018-05-18 17:55:40 -0400

    Ryan was here.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+console.log("RYAN WAS HERE");
+
 
 console.log("blakatah");
 console.log("HI GROUP MEMBERS!");
