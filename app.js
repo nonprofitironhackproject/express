c27ae9acf6febd249533c70a96111922dddd02f6
Ryan Martin <rmart106@fiu.edu>
2018-05-18 16:40:47 -0400

    Test

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+console.log("HI GROUP MEMBERS!");
 
 mongoose.Promise = Promise;
 mongoose
