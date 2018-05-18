8846a85f42b19a3a4c16f55501b2c6c7727100e1
Brian Jaimes <jaimes.brian001@gmail.com>
2018-05-18 16:51:27 -0400

    brianspush

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+console.log("blakatah");
 
 mongoose.Promise = Promise;
 mongoose
