a80adc2ba2d4b1ba7bb060e3ca222c00cd2ef05e
Ryan Martin <rmart106@fiu.edu>
2018-05-20 11:53:08 -0400

    Add: User model and npm installs for bcrypt, passport, flash, axios, ect.

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const axios          = require("axios");
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const cookieParser   = require('cookie-parser');
+const favicon        = require('serve-favicon');
+const hbs            = require('hbs');
+const mongoose       = require('mongoose');
+const logger         = require('morgan');
+const path           = require('path');
+const passport       = require("passport");
+const LocalStrategy  = require("passport-local").Strategy;
+const bcrypt         = require("bcrypt");
+const session        = require("express-session");
 
-console.log("RYAN WAS HERE");
-console.log("RYAN WAS HERE AGAIN");
-console.log("KAT WAS HERE");
-console.log("continue the party");
-
-
-console.log("blakatah");
-console.log("HI GROUP MEMBERS!");
+// Models
+const User         = require('./models/user');
 
 mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/irongenerate-express', {useMongoClient: true})
   .then(() => {
-    console.log('Connected to Mongo!')
-    console.log('kat will kidnap porkchop tonight!')
+    console.log('Connected to Mongo!');
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
@@ -40,28 +38,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
