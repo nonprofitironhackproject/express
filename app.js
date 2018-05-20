b65bea518c1c0bd9636050b2cd9af4cd81534eec
KatAlderman <kat@uxkat.co>
2018-05-20 11:48:32 -0400

    adding auth-routes and cleaning up app.js console.log stuff

--- app.js
+++ app.js
@@ -9,21 +9,11 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-console.log("RYAN WAS HERE");
-console.log("RYAN WAS HERE AGAIN");
-console.log("KAT WAS HERE");
-console.log("continue the party");
-
-
-console.log("blakatah");
-console.log("HI GROUP MEMBERS!");
-
 mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/irongenerate-express', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
-    console.log('kat will kidnap porkchop tonight!')
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
@@ -40,7 +30,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -52,8 +41,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
