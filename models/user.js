693bfbb4944e20a8c2847f351f3834549bfe4a87
KatAlderman <kat@uxkat.co>
2018-05-21 12:52:28 -0400

    Fix: user model

--- models/user.js
+++ models/user.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model('User', UserSchema);
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
     email: String,
     username: String,
-    password: String,    
-    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+    password: String    
+},
+{timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
     
-UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', UserSchema);
+// UserSchema.plugin(passportLocalMongoose);
+// module.exports = mongoose.model('User', UserSchema);
