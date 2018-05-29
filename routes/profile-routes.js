277d0b86fb41d2de6fd3e5dd882ff169aa592ad0
Brian Jaimes <jaimes.brian001@gmail.com>
2018-05-29 10:40:11 -0400

    updating profile routes

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -9,7 +9,7 @@ const express     = require('express');
 const router      = express.Router();
 const User        = require('../models/user'); 
 const Profile     = require('../models/profile'); 
-const loggedIn    = require('../../utils/isAuthenticated');
+// const loggedIn    = require('../../utils/isAuthenticated');
 
 router.get('/', (req, res, next) => {
     Profile
@@ -22,28 +22,28 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', loggedIn, (req, res, next) => {
-    const newThread = new Thread({
-        _author: req.user._id,
-        title: req.body.title,
-        content: req.body.content
-        // profileImage: req.body.profileImage,
-        // backgroundImage: req.body.backgroundImage,
-        // firstName: req.body.firstName,
-        // lastName: req.body.lastName,
-        // aboutUser: req.body.aboutUser,
-        // age: req.body.age,
-        // gender: req.body.gender,
-        // volunteerExperience
-    });
+// router.post('/', loggedIn, (req, res, next) => {
+//     const newThread = new Thread({
+//         _author: req.user._id,
+//         title: req.body.title,
+//         content: req.body.content
+//         profileImage: req.body.profileImage,
+//         backgroundImage: req.body.backgroundImage,
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         aboutUser: req.body.aboutUser,
+//         age: req.body.age,
+//         gender: req.body.gender,
+//         volunteerExperience
+//     });
 
-    newThread.save((err) => {
-        if (err) { return res.status(500).json(err); }
-        if (newThread.errors) { return res.status(400).json(newThread); }
+    // newThread.save((err) => {
+    //     if (err) { return res.status(500).json(err); }
+    //     if (newThread.errors) { return res.status(400).json(newThread); }
 
-        return res.status(200).json(newThread);
-    });
-});
+    //     return res.status(200).json(newThread);
+    // });
+// });
 
 
 module.exports = router;
